4a10b5b141e350142996120330eb09ec05adae36
ikedaS <ikeike616@icloud.com>
2024-03-22 16:30:38 +0900

    edit example2

--- example2.py
+++ example2.py
@@ -14,9 +14,11 @@ pose_model = "C:\Python_Works\AI\mediapipe\model\pose_landmarker_lite.task"
 
 
 def draw_landmarks_on_image(rgb_image, detection_result):
+    print("aaaaaa")
     pose_landmarks_list = detection_result.pose_landmarks
+   
     annotated_image = np.copy(rgb_image)
-
+   
     # Loop through the detected poses to visualize.
     for idx in range(len(pose_landmarks_list)):
         pose_landmarks = pose_landmarks_list[idx]
@@ -54,36 +56,40 @@ while cap.isOpened():
         continue
     image.flags.writeable = False
     #カメラ画像の取得
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+    # mp_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # image = mp.Image.create_from_file(image)
     #姿勢の認識
+    print(image)
     
-    print("================================================")
-    detection_result = detector.detect(image)
-    print("detection_result.pose_landmarks")
-
+    results = detector.detect(mp_image)
+    # print("detection_result.pose_landmarks")
+   
     # 右肩、右手首のランドマークを取得
     # r_shoulder_landmark = results.pose_landmarks.landmark[12] 
     # r_wrist_landmark = results.pose_landmarks.landmark[16]
-
+    # print(results.pose_landmarks)
+    print("================================================")
     # 検出されたポーズの骨格をカメラ画像に重ねて描画
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # image.flags.writeable = True
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     #画像サイズの取得
-    height, width, channels = image.shape[:3]
-    print("width: " + str(width))
-    print("height: " + str(height))
+    # height, width, channels = image.shape[:3]
+    # print("width: " + str(width))
+    # print("height: " + str(height))
 
-    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-
+    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), results)
+    print("================================================")
     # mp_drawing.draw_landmarks(
     #     image,
     #     results.pose_landmarks,
     #     mp_pose.POSE_CONNECTIONS,
     #     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-    cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+    print(annotated_image)
+    # cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+    cv2.imshow('MediaPipe Pose',cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
     # landmark_list = results.pose_landmarks
     # print(landmark_list.landmark[12])
     print("!!!")
